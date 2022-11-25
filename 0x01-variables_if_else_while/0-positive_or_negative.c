08e1dffbbf5cd2bc5d4dd0f820e9893fc8c3eac1
gnkanja <gnkanja@gmail.com>
2022-11-24 23:31:26 -0800

    0-positive_or_negative.c1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,7 @@
 
  */
 
-  
-
-int main(void)
+  int main(void)
 
 {
 
@@ -30,23 +28,17 @@ int main(void)
 
 	n = rand() - RAND_MAX / 2;
 
-	if(n>0) {
+	if (n > 0) {
 
                            print (“%d is positive\n”, n);
 
-              }
-
-                else if (n<0) {
+               else 
 
                            print (“%d is negative\n”, n);
 
-              }
-
-                else if (n=0) {
+              else if (n == 0) 
 
                             print (“%d is zero\n”, n);
-
-             }
 
              return (0);
 
