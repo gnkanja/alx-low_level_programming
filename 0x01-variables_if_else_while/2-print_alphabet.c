00ff034ec96f0fd9abb2300737f84f01aabc18b5
gnkanja <gnkanja@gmail.com>
2022-11-25 04:19:00 -0800

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: printing the alphabet using putchar
+ *Return: Always 0 (Successful)
+ */
+int main()
+{
+	int n;
+	char a;
+
+        a = n; 
+
+	for(n=1;n<=28;n++)
+	{
+		putchar("%c\n", a);
+	}
+	return(0);
+}
